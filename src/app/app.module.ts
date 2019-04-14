58a99e8e7aad904d15249e5184218425e2059c04
ugesh <ugesh.roopchand@hotmail.com>
2019-04-14 14:48:07 +0400

    Component create  and Layout done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,17 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ContentAreaComponent } from './content-area/content-area.component';
+import { FooterComponent } from './footer/footer.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ContentAreaComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
